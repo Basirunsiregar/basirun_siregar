a18b6d455893f1085931aef50d73caba1a68497d
Basirunsiregar <basirunsiregar71@gmail.com>
2024-02-14 15:03:30 +0700

    basirun

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Function to toggle the menu visibility
+function toggleMenu() {
+  var menu = document.getElementById('hamburger-nav');
+  menu.classList.toggle('show-menu');
+}
+
+
+
